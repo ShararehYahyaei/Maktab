afc05cbbf068fe17b33539004fa6292fd1f08d7a
shararaeh <your.email@example.com>
2024-10-29 10:37:42 +0100

    commit Moderator Class For All Moderator Information

---
+++ src/HomeWork/Session5/model/Moderator.java
@@ -0,0 +1,31 @@
+package HomeWork.Session5.model;
+
+public class Moderator {
+    private int id;
+    private String userName;
+    private String passWord;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
